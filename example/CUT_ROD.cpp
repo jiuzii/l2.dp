e239a170f47bbed9f4033e2463e3bff4edcc0a4e
jiuzii <wanghuaiyu94@163.com>
2023-10-26 23:54:41 +0800

    f-commit

--- example/CUT_ROD.cpp
+++ example/CUT_ROD.cpp
@@ -9,7 +9,7 @@ int MEMOIZED_CUT_ROD(int p[ARRAYLEN],int n,int r[ARRAYLEN]);
 int main(){
 
     int p[ARRAYLEN] = {1,5,8,9,10,17,17,20,24,30};
-    int r[ARRAYLEN+1] = {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1};
+    int r[ARRAYLEN] = {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1};
     auto start = std::chrono::high_resolution_clock ::now();
     std::cout << CUT_ROD(p,ARRAYLEN) << std::endl;
     auto end = std::chrono::high_resolution_clock ::now();
@@ -31,33 +31,36 @@ int CUT_ROD(int p[ARRAYLEN],int n){
         return 0;
     }
     q = -1;
-    for( int i=0;i<n;i++){
-        q = std::max(q,p[i]+CUT_ROD(p,n-i-1));
+    for( int i=1;i<=n;i++){
+        q = std::max(q,p[i-1]+CUT_ROD(p,n-i));
     }
     return q;
 }
 
 
 int MEMOIZED_CUT_ROD(int p[ARRAYLEN],int n,int r[ARRAYLEN]){
-  int q;
-  if( n==1 ){
-    r[n] = p[0];
-    std::cout << n <<" "<< r[n]<<std::endl;
-    return p[0];
-  }
-  if(n==0){
-    r[0]=0;
-    return 0;
-  }
-  q = -1;
-  for( int i=0;i<n;i++){
-    if( n >1 && r[n-1]>0){
-      r[n] = p[i] + r[n-i-1];
-    } else{
-      q = std::max(q,p[i]+MEMOIZED_CUT_ROD(p,n-i-1,r));
-      r[n]=q;
-      std::cout << n <<" "<< r[n]<<std::endl;
+    int q;
+    if( n==0 ){
+        std::cout << 0 << std::endl;
+        return 0;
     }
-  }
-  return r[n];
+    if( n==1 ){
+        std::cout << r[0] << std::endl;
+        r[0] = p[0];
+        return r[0];
+    }
+    q = -1;
+    for( int i=1;i<=n;i++){
+        if(n-i > 0 && r[n-i-1] > 0 ){
+            q = std::max(q,p[i-1]+r[n-i-1]);
+        }else{
+            q = std::max(q,p[i-1]+MEMOIZED_CUT_ROD(p,n-i,r));
+
+        }
+
+    }
+    r[n-1] = q;
+    std::cout<< n-1 << " "<<q <<std::endl;
+    return q;
+
 }
